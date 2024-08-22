8fab6663654145e0376216c0b393c47f1603d696
Eric Fernandes Pereira <83287307+EricFernandes26@users.noreply.github.com>
2024-08-22 16:14:33 -0300

    Update instances.py

--- instances.py
+++ instances.py
@@ -1,22 +1,39 @@
 def recomendar_instancia_mysql(metricas):
-    # Critérios para recomendação de instância baseado em CPU, Memória e I/O
-    if metricas['threads_running'] < 5 and metricas['queries'] < 1000:
-        return 't3.small'
-    elif metricas['threads_running'] < 10 and metricas['queries'] < 5000:
-        return 't3.medium'
-    elif metricas['threads_running'] < 15 and metricas['queries'] < 10000:
-        return 't3.large'
-    elif metricas['buffer_reads'] > 1000000 or metricas['buffer_writes'] > 500000:
-        return 'm5.xlarge'
-    elif metricas['buffer_reads'] > 5000000 or metricas['buffer_writes'] > 2500000:
-        return 'm5.2xlarge'
-    elif metricas['data_reads'] > 1000000 or metricas['data_writes'] > 500000:
-        return 'r6i.4xlarge'
-    elif metricas['data_reads'] > 5000000 or metricas['data_writes'] > 2500000:
-        return 'r6i.8xlarge'
-    elif metricas['data_reads'] > 10000000 or metricas['data_writes'] > 5000000:
-        return 'r6i.12xlarge'
-    elif metricas['data_reads'] > 20000000 or metricas['data_writes'] > 10000000:
-        return 'x2iedn.16xlarge'
+    # Inicializar uma variável de pontuação
+    pontuacao = 0
+
+    # Pesos para cada métrica
+    peso_threads = 1.5
+    peso_queries = 1.2
+    peso_buffer = 1.8
+    peso_io = 2.0
+
+    # Adicionar pontuação baseada nas métricas
+    pontuacao += (metricas['threads_running'] * peso_threads)
+    pontuacao += (metricas['queries'] * peso_queries)
+    pontuacao += ((metricas['buffer_reads'] + metricas['buffer_writes']) * peso_buffer)
+    pontuacao += ((metricas['data_reads'] + metricas['data_writes']) * peso_io)
+
+    # Definir faixas de pontuação para recomendar a instância
+    if pontuacao < 5000:
+        instancia = 't3.small'
+    elif pontuacao < 10000:
+        instancia = 't3.medium'
+    elif pontuacao < 20000:
+        instancia = 'm5.large'
+    elif pontuacao < 40000:
+        instancia = 'm5.xlarge'
+    elif pontuacao < 80000:
+        instancia = 'm5.2xlarge'
+    elif pontuacao < 160000:
+        instancia = 'r6i.4xlarge'
+    elif pontuacao < 320000:
+        instancia = 'r6i.8xlarge'
+    elif pontuacao < 640000:
+        instancia = 'r6i.12xlarge'
+    elif pontuacao < 1280000:
+        instancia = 'x2iedn.16xlarge'
     else:
-        return 'x2iedn.24xlarge'
+        instancia = 'x2iedn.24xlarge'
+
+    return instancia, pontuacao
